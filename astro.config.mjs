4407ba2b40d86b296a907116de5a395d52cdf85d
gbourguett <gbourguett@lovalledor.cl>
2024-12-26 08:37:05 -0300

    SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,24 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
+
+
+
+import sitemap from '@astrojs/sitemap';
+
+
+
+import robotsTxt from 'astro-robots-txt';
 
 
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://nye.today',
   integrations: [tailwind(), react({
       include: ['**/react/*'], experimentalReactChildren: true
-    })],
+    }), sitemap(), robotsTxt()],
   adapter: vercel({
     webAnalytics: {
       enabled: true,
